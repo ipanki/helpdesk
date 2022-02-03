1a9aa70853a8e0ebf946aebb1fbb979a37c619e7
Daniil Panko <daniil.panko@mail.ru>
2022-02-03 03:03:14 +0300

    Working version

---
+++ issues/tasks.py
@@ -0,0 +1,27 @@
+from celery import shared_task
+
+from .models import Issue
+
+
+@shared_task
+def pause_issue(issue_id):
+    issue = Issue.objects.get(id=issue_id)
+    issue.pause()
+    issue.save()
+    return True
+
+
+@shared_task
+def resolve_issue(issue_id):
+    issue = Issue.objects.get(id=issue_id)
+    issue.resolve()
+    issue.save()
+    return True
+
+
+@shared_task
+def reopen_issue(issue_id):
+    issue = Issue.objects.get(id=issue_id)
+    issue.reopen()
+    issue.save()
+    return True
